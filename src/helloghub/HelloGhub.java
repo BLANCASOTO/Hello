acd80967803b5528bc17bf215f520665d6678c6c
BLANCASOTO <1715110171@utec-tgo.mx>
2016-10-06 12:02:02 -0500

    first commit

---
+++ src/helloghub/HelloGhub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloghub;
+
+/**
+ *
+ * @author Bnc
+ */
+public class HelloGhub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] Bfs) {
+    System.out.print ("Hola muy buenos dias ");
+
+// TODO code application logic here
+    }
+    
+}
